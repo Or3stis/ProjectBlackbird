6fd19e07d86f26284764493ef73c591e1739c953
Or3stis <chairman_spam@yahoo.com>
2015-10-07 23:40:18 +0100

    Create hidden_ssid.py

---
+++ wireless/hidden_ssid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/ python3
+
+# hidden SSID sniffer
+# python 3
+
+from scapy.all import *
+
+# selection of the interface
+iface = str(input('\n Enter interface: '))
+
+
+# enables scapy to sniff for probes
+def handle_packet(packet):
+    # scans only for request, response and assosiation probes
+    if packet.haslayer(Dot11ProbeReq) or packet.haslayer(Dot11ProbeResp) \
+        or packet.haslayer(Dot11AssoReq):
+        # prints the SSIDs 
+        print('Found SSID ' + packet.info)
+
+# enable sniffing
+print('sniffing on ' + iface + '...')
+sniff(iface=iface, prn=handle_packet)
